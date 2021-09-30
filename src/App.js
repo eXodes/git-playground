1829c85e91601df9398277ad1bbb4e09432fe97a
Exo Des <hello@exodes.net>
2021-09-30 20:31:01 +0800

    add react use (#5)
    * fix: update title for git repo
    
    * build: add `react-use`
    
    * feat: add online status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useNetworkState } from "react-use";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 function App() {
+  const { online } = useNetworkState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+
+        <p>Currently is {online ? "😀" : "😐"}</p>
+        <motion.img
+          src={logo}
+          initial='hidden'
+          animate='visible'
+          variants={variants}
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
