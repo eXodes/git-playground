04ccf1ab8e20d4d386652aa5efc2786cf43573f1
Exo Des <hello@exodes.net>
2021-09-30 22:27:41 +0800

    Add library (#17)
    * build: add `framer-motion`
    
    * feat: add motion to image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <motion.img
+          src={logo}
+          initial='hidden'
+          animate='visible'
+          variants={variants}
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Handling complex Git flow is hard!
         </p>
         <small>Not sure if I can do this right.</small>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
