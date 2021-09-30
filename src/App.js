d6ea41a564208f60dac3be0855745f190c3e502a
Exo Des <hello@exodes.net>
2021-09-30 20:08:16 +0800

    feat: add motion to image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <motion.img
+          src={logo}
+          initial='hidden'
+          animate='visible'
+          variants={variants}
+          className='App-logo'
+          alt='logo'
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
