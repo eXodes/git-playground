e8225dd49fb493459c4d011ef2df6e64d1a5353f
Exo Des <hello@exodes.net>
2021-10-01 21:32:24 +0800

    feat: add counter (#23)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,8 @@ function App() {
           Handling complex Git flow is hard!
         </p>
         <small>Not sure if I can do this right.</small>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
